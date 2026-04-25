0ce74af293e5ca7b117f29dc13dfeeef24c0e10b
Helder Martins <edaojs@gmail.com>
2026-04-25 19:37:36 -0300

    static map

---
+++ assets/world/greeCollision.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="greeCollision" tilewidth="23" tileheight="23" tilecount="4" columns="2">
+ <image source="square_64.png" width="64" height="64"/>
+</tileset>
---
+++ assets/world/tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="tileset" tilewidth="23" tileheight="23" tilecount="1947" columns="59">
+ <image source="tileset.png" width="1376" height="768"/>
+</tileset>
